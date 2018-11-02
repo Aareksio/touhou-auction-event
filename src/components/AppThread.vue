fa89cbb1ba61b04affade9a9db6582a161dc397b
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-11-02 02:33:34 +0100

    Open links in new tab

--- src/components/AppThread.vue
+++ src/components/AppThread.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="thread" v-if="thread.isOver"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.bidLocaleTime }} - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL">{{ thread.winner.username }}</a></li>
+  <li class="thread" v-if="thread.isOver"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.bidLocaleTime }} - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL" target="_blank">{{ thread.winner.username }}</a></li>
   <li class="thread" v-else-if="thread.winner.steamid"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.timeAgo }} - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL" target="_blank">{{ thread.winner.username }}</a></li>
   <li class="thread" v-else><span v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.timeAgo }} - <span v-html="thread.bidHTML"></span> credits</li>
 </template>
