737784f8267f90957cf13637f095894a54c264d5
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-11-02 02:37:40 +0100

    Update order

--- src/components/AppThread.vue
+++ src/components/AppThread.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="thread" v-if="thread.isOver"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.bidLocaleTime }} - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL" target="_blank">{{ thread.winner.username }}</a></li>
-  <li class="thread" v-else-if="thread.winner.steamid"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.timeAgo }} - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL" target="_blank">{{ thread.winner.username }}</a></li>
-  <li class="thread" v-else><span v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - {{ thread.timeAgo }} - <span v-html="thread.bidHTML"></span> credits</li>
+  <li class="thread" v-else-if="thread.winner.steamid"><span class="round-id" v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - <span v-html="thread.bidHTML"></span> credits by <a :href="thread.winnerURL" target="_blank">{{ thread.winner.username }}</a> - {{ thread.timeAgo }}</li>
+  <li class="thread" v-else><span v-html="thread.idHTML"></span> - <a :href="thread.threadURL" target="_blank">visit</a> - <span v-html="thread.bidHTML"></span> credits - {{ thread.timeAgo }}</li>
 </template>
 
 <script>
